00731dae9a71edfabba24d397c5ad02f9653a36e
Ewelina <ewelina2502@gmail.com>
2022-04-01 14:01:05 +0200

    add cookies in tests olx

--- src/test/java/olx/OlxMethods.java
+++ src/test/java/olx/OlxMethods.java
@@ -2,11 +2,17 @@ package olx;
 
 import methods.WebdriverPath;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.Test;
+
+
+import java.io.*;
+import java.util.Date;
+import java.util.StringTokenizer;
+
 
 public class OlxMethods extends OlxSelectors {
 
@@ -28,7 +34,68 @@ public class OlxMethods extends OlxSelectors {
         actions.moveToElement(menuOption).perform();
         WebElement subMenuOption = driver.findElement(By.xpath(wylogujSie));
         actions.moveToElement(subMenuOption).click().perform();
-        driver.quit();
 
+        driver.quit();
     }
+
+    public static void testCookies() throws InterruptedException {
+
+        WebdriverPath.startMethod();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get(webSiteOlx);
+        driver.findElement(By.xpath(acceptCookies)).click();
+        driver.findElement(By.xpath(myOlxButton)).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id(userEmail)).sendKeys("ewelina2502@o2.pl");
+        driver.findElement(By.id(userPassword)).sendKeys("Testowy123");
+        driver.findElement(By.id(zalogujSie)).click();
+
+
+        try{
+        File file = new File("Cookiefile.data");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader Buffreader = new BufferedReader(fileReader);
+        String strline;
+        while((strline=Buffreader.readLine())!=null){
+            StringTokenizer token = new StringTokenizer(strline,";");
+            while(token.hasMoreTokens()){
+                String name = token.nextToken();
+                String value = token.nextToken();
+                String domain = token.nextToken();
+                String path = token.nextToken();
+                Date expiry = null;
+
+                String val;
+                if(!(val=token.nextToken()).equals("null"))
+                {
+                    expiry = new Date(val);
+                }
+                Boolean isSecure = Boolean.parseBoolean(token.nextToken());
+                Cookie ck = new Cookie(name,value,domain,path,expiry,isSecure);
+
+                System.out.println(ck);
+                driver.manage().addCookie(ck);
+            }
+        }
+    } catch (Exception ex){
+        ex.printStackTrace();
+    }
+        driver.get(webSiteOlx);
+
+
 }
+
+
+
+
+
+
+
+
+	}
+
+
+
+
--- src/test/java/olx/OlxTests.java
+++ src/test/java/olx/OlxTests.java
@@ -1,7 +1,9 @@
 package olx;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static olx.OlxMethods.testCookies;
 import static olx.OlxMethods.testLogToOlx;
 
 public class OlxTests {
@@ -10,6 +12,12 @@ public class OlxTests {
     public void test_positive_log_olx() throws InterruptedException {
         testLogToOlx();
     }
+
+    @Test
+    public void test_cookies_olx() throws InterruptedException {
+        testCookies();
+    }
+
 
 
 
