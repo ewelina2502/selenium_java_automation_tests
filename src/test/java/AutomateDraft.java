d25074c254c2896e4f26d63ddcd35c21d1968276
ewelina2502 <piotrowska.ewelina@gmail.com>
2022-04-05 14:32:35 +0200

    add dragAnddrop options

--- src/test/java/AutomateDraft.java
+++ src/test/java/AutomateDraft.java
@@ -1,9 +1,42 @@
+import methods.WebdriverPath;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class AutomateDraft {
 
+    @Test
+    public void movingelements() {
+
+            WebdriverPath.startMethod();
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+
+            String url = "https://selenium08.blogspot.com/2020/01/click-and-hold.html";
+            driver.get(url);
+
+            WebElement cookies = driver.findElement(By.id("cookieChoiceDismiss"));
+            WebElement titleA = driver.findElement(By.xpath("//li[text()= 'A']"));
+            WebElement titleC = driver.findElement(By.xpath("//li[text()= 'C']"));
+            cookies.click();
+
+            Actions actions = new Actions(driver);
 
 
+            actions.moveToElement(titleA);
+//            actions.clickAndHold();
+//            actions.moveToElement(titleC);
+//            actions.clickAndHold();
 
+        actions.dragAndDrop(titleC,titleA);
+            actions.release().perform();
+
+//            driver.quit();
+        }
 }
--- src/test/java/methods/WebdriverPath.java
+++ src/test/java/methods/WebdriverPath.java
@@ -3,8 +3,10 @@ package methods;
 public class WebdriverPath {
 
     public static void startMethod() {
+
         String webDriverChromeDriver = "webdriver.chrome.driver";
-        String chromeBrowserExePath = "C:\\Users\\Ewelina\\Downloads\\chromedriver.exe";
+        String chromeBrowserExePath = "C:\\Users\\eweli\\Downloads\\chromedriver.exe";
+
         System.setProperty(webDriverChromeDriver, chromeBrowserExePath);
 
     }
